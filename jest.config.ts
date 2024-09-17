e6ae6b14e32eaab86da8cc6357ee18fadb5439d3
luizProgHenri <luiz.henrique@arenafantasy.com>
2024-09-17 14:51:09 -0300

    Setup

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+// jest.config.ts
+import type { Config } from "jest";
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8"
+};
+
+export default config;
