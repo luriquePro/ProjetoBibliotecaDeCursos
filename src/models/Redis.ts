30fd1e1c4a4db39baf6312df19497ae24c4897de
LuizProgHenri <luiz.henrique@arenafantasy.com>
2024-10-06 17:21:35 -0300

    Ajustes Redis

--- src/models/Redis.ts
+++ src/models/Redis.ts
@@ -1,5 +1,8 @@
 import { createClient } from "redis";
 
-const client = createClient({ url: "redis://127.0.0.1:6379", database: Number(process.env.REDIS_DB_CONNECTION_DATABASE) });
+const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
+const REDIS_PORT = process.env.REDIS_PORT || "6379";
+
+const client = createClient({ url: `redis://${REDIS_HOST}:${REDIS_PORT}`, database: Number(process.env.REDIS_DB_CONNECTION_DATABASE) });
 
 export { client };
